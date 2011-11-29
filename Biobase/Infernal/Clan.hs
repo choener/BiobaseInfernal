d613b6c35ef41a26814138fd1e24f5210d3822fe
Christian Hoener zu Siederdissen <choener@tbi.univie.ac.at>
2011-11-29 19:59:44 +0100

    improvements on clan representation

--- Biobase/Infernal/Clan.hs
+++ Biobase/Infernal/Clan.hs
@@ -1,9 +1,7 @@
 {-# LANGUAGE OverloadedStrings #-}
 
 -- | Rfam clans are a set of biologically related Rfam families. This module
--- provides simple abstraction methods and loaders.
---
--- TODO This has to go into biobase and needs to be made nice-looking.
+-- provides simple abstraction methods and loaders from file and ByteString.
 --
 -- TODO load and parse with enumerator
 
@@ -13,13 +11,16 @@ import qualified Data.ByteString.Char8 as BS
 
 
 
--- | Simple RfamClan data wrapper. Could Easily be just a list of bytestrings,
--- which it is using strings
+-- | Simple Rfam clan data.
 
 data Clan = Clan
-  { accession :: BS.ByteString
+  -- | result of the "AC    CL00001" line, keeping "1" in this case.
+  { accession  :: Int -- BS.ByteString
+  -- | the "ID    tRNA" line, keeping "tRNA".
   , identifier :: BS.ByteString
-  , members :: [BS.ByteString]
-  , strings :: [BS.ByteString]
+  -- | all the "MB    RF00005;", "MB    RF00023;" lines, keeping "[5,23]".
+  , members    :: [Int] -- [BS.ByteString]
+  -- | all lines of each clan, without any processing (except being in lines).
+  , strings    :: [BS.ByteString]
   } deriving (Read,Show,Eq)
 
--- Biobase/Infernal/Clan/Import.hs
+++ Biobase/Infernal/Clan/Import.hs
@@ -13,20 +13,30 @@ import Biobase.Infernal.Clan
 
 
 
--- | Import the complete data strictly.
+-- | Import the complete data from an uncompressed source file.
 
 fromFile :: FilePath -> IO [Clan]
-fromFile fp = do
-  (map mkClan . groupBy (\x y -> "AC"/=(head . BS.words $y)) . BS.lines) `fmap` BS.readFile fp
+fromFile fp = fromByteString `fmap` BS.readFile fp
+
+-- | Transform a bytestring into a list of 'Clan's.
+
+fromByteString :: BS.ByteString -> [Clan]
+fromByteString s = map mkClan
+                 . groupBy (\x y -> "AC"/=(head . BS.words $y))
+                 . BS.lines
+                 $ s
 
 -- | Given a list of bytestrings, create one Clan.
 --
--- TODO return Maybe, make crash-safe
+-- TODO return Maybe, make crash-safe (not really high on the list...)
 
 mkClan :: [BS.ByteString] -> Clan
 mkClan xs = Clan
-  { accession  = (!!1) . BS.words . head . filter ((=="AC") . BS.take 2) $ xs
+  { accession  = f . BS.drop 2 . (!!1) . BS.words . head . filter ((=="AC") . BS.take 2) $ xs
   , identifier = (!!1) . BS.words . head . filter ((=="ID") . BS.take 2) $ xs
-  , members    = map BS.init . map (!!1) . filter ((=="MB") . (!!0)) . map BS.words $ xs
+  , members    = map (f . BS.drop 2 . BS.init . (!!1)) . filter ((=="MB") . (!!0)) . map BS.words $ xs
   , strings    = xs
-  }
+  } where
+      f s
+        | Just (k, _) <- BS.readInt s = k
+        | otherwise = error $ "mkClan: " ++ BS.unpack s
