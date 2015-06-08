8c45f0ecdf3d5a46a91421cc5fd2d0b8aeeb9652
Christian Hoener zu Siederdissen <choener@tbi.univie.ac.at>
2015-06-09 00:17:25 +0200

    ghc 7.8.4 compatibility

--- tests/properties.hs
+++ tests/properties.hs
@@ -1,13 +1,14 @@
 
 module Main where
 
+import Control.Lens
+import Data.Monoid (mempty)
 import Test.Framework.Providers.HUnit
 import Test.Framework.Providers.QuickCheck2
 import Test.Framework.TH
 import Test.HUnit
 import Test.QuickCheck.Modifiers
 import Test.QuickCheck.Property
-import Control.Lens
 
 import Biobase.SElab.HMM
 
