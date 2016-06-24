879c4f819eafd86fe2ea9516c7d9411ec6146aa2
Christian Hoener zu Siederdissen <choener@bioinf.uni-leipzig.de>
2016-06-25 01:03:59 +0200

    moving out ADPfusion parts

--- tests/properties.hs
+++ tests/properties.hs
@@ -3,12 +3,13 @@ module Main where
 
 import           Control.Lens
 import           Data.Monoid (mempty)
-import           Test.Framework.Providers.HUnit
-import           Test.Framework.Providers.QuickCheck2
-import           Test.Framework.TH
 import           Test.HUnit
 import           Test.QuickCheck.Modifiers
 import           Test.QuickCheck.Property
+import           Test.Tasty
+import           Test.Tasty.HUnit (testCase)
+import           Test.Tasty.QuickCheck (testProperty)
+import           Test.Tasty.TH
 
 import qualified Biobase.SElab.HMM as HMM
 import qualified Biobase.SElab.CM as CM
