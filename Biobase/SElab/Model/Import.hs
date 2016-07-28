5e030b856919cae3ce635674b1f5084cc0e8b1da
Christian Hoener zu Siederdissen <choener@bioinf.uni-leipzig.de>
2016-07-28 19:37:40 +0200

    ok, but not perfect for general model handling

--- Biobase/SElab/Model/Import.hs
+++ Biobase/SElab/Model/Import.hs
@@ -1,6 +1,7 @@
 
 module Biobase.SElab.Model.Import where
 
+import           Control.Lens (set)
 import           Data.Conduit
 import qualified Data.Conduit.List as CL
 import qualified Data.Map.Strict as M
@@ -17,5 +18,17 @@ import           Biobase.SElab.Model.Types
 -- TODO run within a logger monad
 
 attachHMMs :: (Monad m) => Conduit (Either (HMM ()) CM) m CM
-attachHMMs = undefined
+attachHMMs = CL.head >>= go
+  where go Nothing = return ()
+        go (Just (Right cm)) = do
+          mhmm <- CL.peek
+          case mhmm of
+            Nothing -> yield cm >> return ()
+            Just (Left h) -> do
+              CL.drop 1
+--              yield $ set hmm h cm
+              CL.head >>= go
+            Just (Right cm') -> do
+              yield cm
+              go (Just (Right cm'))
 
