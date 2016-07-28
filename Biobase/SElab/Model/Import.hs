53325eb75f9ae148a1d10a58f0c68b30e6f70d2c
Christian Hoener zu Siederdissen <choener@bioinf.uni-leipzig.de>
2016-07-28 19:22:55 +0200

    still thinking

--- Biobase/SElab/Model/Import.hs
+++ Biobase/SElab/Model/Import.hs
@@ -1,5 +1,21 @@
 
 module Biobase.SElab.Model.Import where
 
-import Biobase.SElab.Model.Types
+import           Data.Conduit
+import qualified Data.Conduit.List as CL
+import qualified Data.Map.Strict as M
+
+import           Biobase.SElab.CM.Types
+import           Biobase.SElab.HMM.Types
+import           Biobase.SElab.Model.Types
+
+
+
+-- | Combine CMs with their HMMs. Assumes that each CM is followed by its
+-- HMM.
+--
+-- TODO run within a logger monad
+
+attachHMMs :: (Monad m) => Conduit (Either (HMM ()) CM) m CM
+attachHMMs = undefined
 
