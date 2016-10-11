553f56a758e5994578f8017ad5bc2f5fe3fd6337
Christian Hoener zu Siederdissen <choener@bioinf.uni-leipzig.de>
2016-10-11 18:32:29 +0200

    prepare for my flexible model structures

---
+++ Biobase/SElab/CM/ModelStructure.hs
@@ -0,0 +1,8 @@
+
+-- | Defines two model structures. One structure is designed to be easily
+-- modifiable for working with a CM. The second is "static" but efficient
+-- to use in applications. An isomorphism between the two structures is
+-- provided.
+
+module Biobase.SElab.CM.ModelStructure where
+
