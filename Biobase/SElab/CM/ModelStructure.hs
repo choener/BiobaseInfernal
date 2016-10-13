c5ceda657c0416c7ed249d5bb14e5e3d3457ca06
Christian Hoener zu Siederdissen <choener@bioinf.uni-leipzig.de>
2016-10-13 18:32:19 +0200

    new static model design
    still needs to be benchmarked

--- Biobase/SElab/CM/ModelStructure.hs
+++ Biobase/SElab/CM/ModelStructure.hs
@@ -11,22 +11,23 @@
 
 module Biobase.SElab.CM.ModelStructure where
 
-import qualified Data.Vector as V
-import qualified Data.Vector.Generic as VG
-import           GHC.Generics (Generic)
+import           Control.DeepSeq
+import           Control.Lens
+import           Data.Aeson (FromJSON,ToJSON)
 import           Data.Binary (Binary)
 import           Data.Default
 import           Data.Hashable (Hashable)
-import           Data.Serialize (Serialize)
-import           Data.Aeson (FromJSON,ToJSON)
-import           Control.DeepSeq
 import           Data.Ix (Ix)
-import           Text.Read
-import           Data.Vector.Unboxed.Deriving
-import           Control.Lens
-import           Data.Set (Set)
 import           Data.Map (Map)
+import           Data.Serialize (Serialize)
+import           Data.Set (Set)
+import           Data.Vector.Unboxed.Deriving
+import           GHC.Generics (Generic)
 import qualified Data.Map as M
+import qualified Data.Vector as V
+import qualified Data.Vector.Generic as VG
+import qualified Data.Vector.Unboxed as VU
+import           Text.Read
 
 import           Biobase.Primary.Letter
 import           Biobase.Primary.Nuc.RNA
@@ -35,9 +36,19 @@ import           Data.PrimitiveArray hiding (fromList,toList)
 
 
 
+-- * General things
+
+-- | Phantom-type a node index of @PInt@s
+
 data NodeIndex
 
+-- | Phantom-type a state index @PInt@s
+
+data StateIndex
+
 -- | The type of a node, efficiently encoded as an Int.
+
+
 
 newtype NodeType = NodeType Int
   deriving (Eq,Ord,Generic,Ix)
@@ -87,10 +98,6 @@ derivingUnbox "NodeType"
   [t| NodeType -> Int |] [| \(NodeType n) -> n |] [| NodeType |]
 
 
-
-data StateIndex
-
-type Transitions b = Vector (PInt () StateIndex, b)
 
 -- | Type of a state, a newtype wrapper for performance
 
@@ -150,138 +157,6 @@ derivingUnbox "StateType"
 
 
 
--- * High-performance structure for @Node@s.
-
-data Nodes = Nodes
-  { _nNodesType   :: ! (Unboxed (PInt () NodeIndex) NodeType)
-  , _nNodesStates :: ! (Boxed   (PInt () NodeIndex) (V.Vector (PInt () StateIndex)))
-  , _nNodesColL   :: ! (Unboxed (PInt () NodeIndex) Int)
-  , _nNodesColR   :: ! (Unboxed (PInt () NodeIndex) Int)
-  , _nNodesRefL   :: ! (Unboxed (PInt () NodeIndex) Char)
-  , _nNodesRefR   :: ! (Unboxed (PInt () NodeIndex) Char)
-  }
-  deriving (Eq,Show,Read,Generic)
-
-makeLenses ''Nodes
-makePrisms ''Nodes
-
-instance Default Nodes where
-  def = Nodes
-    { _nNodesType   = fromAssocs 0 0 (NodeType $ -1) []
-    , _nNodesStates = fromAssocs 0 0 VG.empty        []
-    , _nNodesColL   = fromAssocs 0 0 (-1)            []
-    , _nNodesColR   = fromAssocs 0 0 (-1)            []
-    , _nNodesRefL   = fromAssocs 0 0 '-'             []
-    , _nNodesRefR   = fromAssocs 0 0 '-'             []
-    }
-
-instance Binary    Nodes
-instance Serialize Nodes
-instance FromJSON  Nodes
-instance ToJSON    Nodes
-instance NFData    Nodes
-
-
-
-
--- * High-performance structure for @State@s. Actual calculations are run
--- on these.
-
--- | Encode all the information necessary to have *efficient* covariance
--- models.
---
--- Transitions are encoded as a boxed vector of unboxed vectors. The outer
--- boxed vector is indexed by the current state. The inner unboxed vector
--- is indexed by the child number. For each child number we record the
--- target state and transition cost.
---
--- TODO emissions pair/single
--- TODO local / global mode
--- TODO add QDB information here?
---
--- TODO We need to modify how BiobaseXNA encodes RNA sequences (maybe ACGUN)
-
-data States = States
-  { _sTransitions     :: ! (V.Vector (Transitions Bitscore))
-  -- ^ Transitions to a state, together with the transition score;
-  -- unpopulated transitions are set to @-1@.
-  -- TODO we have "forbidden" transitions. Consider how to handle these.
-  -- TODO turn this into a @Boxed (Z:.PInt () StateIndex) (Transitions
-  -- Bitscore)@ ?
-  , _sPairEmissions   :: ! (Unboxed (Z:.PInt () StateIndex:.Letter RNA:.Letter RNA) Bitscore)
-  -- ^ Scores for the emission of a pair
-  , _sSingleEmissions :: ! (Unboxed (Z:.PInt () StateIndex:.Letter RNA) Bitscore)
-  -- ^ Scores for the emission of a single nucleotide
-  , _sStateType       :: ! (Unboxed (PInt () StateIndex) StateType)
-  -- ^ Type of the state at the current index
-  }
-  deriving (Eq,Show,Read,Generic)
-
-makeLenses ''States
-makePrisms ''States
-
--- | A pure getter to retrieve the last state
-
-sLastState :: Getter States (PInt () StateIndex)
-sLastState = sStateType . to bounds . to snd
-{-# Inline sLastState #-}
-
-instance Default States where
-  def = States
-    { _sTransitions     = V.empty
-    , _sPairEmissions   = fromAssocs (Z:.0:.A:.A) (Z:.0:.A:.A) 0                 []
-    , _sSingleEmissions = fromAssocs (Z:.0:.A)    (Z:.0:.A)    0                 []
-    , _sStateType       = fromAssocs 0            0            (StateType $ -1)  []
-    }
-
-instance Binary    States
-instance Serialize States
-instance FromJSON  States
-instance ToJSON    States
-instance NFData    States
-
-
-
--- * Nodes for dynamically changeable models.
-
--- | @Node@s are a high-level structure in covariance models, with each
--- node having one or more states as children. In addition, nodes carry
--- alignment-column based information.
---
--- TODO @_nColL@ and @nColR@ should become @Index 1@ types. We'll do that
--- once we re-activate Stockholm file parsing.
-
-data Node = Node
-  { _nType   :: ! NodeType
-  -- ^ Type of the node
-  , _nStates :: ! (V.Vector (PInt () StateIndex))
-  -- ^ States associated with this node
-  , _nColL   :: ! Int
-  -- ^ Column index in the corresponding Stockholm file
-  , _nColR   :: ! Int
-  -- ^ Column index in the corresponding Stockholm file
-  , _nConL   :: ! Char
-  -- ^ TODO
-  , _nConR   :: ! Char
-  -- ^ TODO
-  , _nRefL   :: ! Char
-  -- ^ TODO
-  , _nRefR   :: ! Char
-  -- ^ TODO
-  }
-  deriving (Eq,Ord,Show,Read,Generic)
-
-makeLenses ''Node
-makePrisms ''Node
-
-instance Binary    Node
-instance Serialize Node
-instance FromJSON  Node
-instance ToJSON    Node
-instance NFData    Node
-
-
-
 -- * QDB parameters.
 
 -- | Query-dependent banding parameters. The four parameters are given in
@@ -317,25 +192,30 @@ derivingUnbox "QDB"
 
 
 
+type Transitions b = VU.Vector (PInt () StateIndex, b)
+
+
+
 -- | A single state in a model.
 --
 -- TODO Map (PInt () StateIndex) State
 
 data State = State
-  { _sType        :: ! StateType
+  { _stateType        :: ! StateType
     -- ^ The type of the current state
-  , _sParents     :: ! (Vector (PInt () StateIndex))
+  , _stateParents     :: ! (VU.Vector (PInt () StateIndex))
     -- ^ List of parents into this state
-  , _sqdb         :: ! QDB
+  , _stateQDB         :: ! QDB
     -- ^ QDB information
-  , _transitions  :: ! (Transitions Bitscore)
+  , _stateTransitions :: ! (Transitions Bitscore)
     -- ^ Into which children to we transition to
-  , _emissions    :: ! (Vector Bitscore)
+  , _stateEmissions   :: ! (VU.Vector Bitscore)
     -- ^ Finally, emission scores, if given for this state. Different
     -- stochastic models should interpret this differently!
     -- For covariance models, the emission order is ACGU for single states
     -- or AA,AC,AG,AU, CA,CC,CG,CU, GA,GC,GG,GU, UA,UC,UG,UU for pair
     -- states.
+    -- TODO really only one entry?
   }
   deriving (Eq,Show,Read,Generic)
 
@@ -344,11 +224,11 @@ makePrisms ''State
 
 instance Default State where
   def = State
-    { _sType        = StateType (-1)
-    , _sParents     = VG.empty
-    , _sqdb         = def
-    , _transitions  = VG.empty
-    , _emissions    = VG.empty
+    { _stateType        = StateType (-1)
+    , _stateParents     = VG.empty
+    , _stateQDB         = def
+    , _stateTransitions = VG.empty
+    , _stateEmissions   = VG.empty
     }
 
 instance Binary    State
@@ -358,6 +238,215 @@ instance ToJSON    State
 instance NFData    State
 
 
+
+-- * High-performance structure for @State@s. Actual calculations are run
+-- on these.
+
+-- | Encode all the information necessary to have *efficient* covariance
+-- models.
+--
+-- The index @PInt () StateIndex@ is the actual index type as given in
+-- a model description.
+--
+-- Transitions are encoded as a boxed vector of unboxed vectors. The outer
+-- boxed vector is indexed by the current state. The inner unboxed vector
+-- is indexed by the child number. For each child number we record the
+-- target state and transition cost.
+--
+-- TODO emissions pair/single
+-- TODO local / global mode
+-- TODO add QDB information here?
+--
+-- TODO We need to modify how BiobaseXNA encodes RNA sequences (maybe ACGUN)
+--
+-- TODO ugly but more efficient? Use just a single @Emit@ data structure?
+
+data States = States
+  { _statesType        :: ! (Unboxed (PInt () StateIndex) StateType)
+  -- ^ Type of the state at the current index
+  , _statesParents     :: ! (Boxed (PInt () StateIndex) (VU.Vector (PInt () StateIndex)))
+  -- ^ For each state, record which other states lead here
+  , _statesTransitions :: ! (Boxed (PInt () StateIndex) (Transitions Bitscore))
+  -- ^ Transitions to a state, together with the transition score;
+  -- unpopulated transitions are set to @-1@.
+  -- TODO we have "forbidden" transitions. Consider how to handle these.
+  -- Easy solution is very low bitscores, maybe @-neginf@?
+  , _statesQDB          :: ! (Unboxed (PInt () StateIndex) QDB)
+  , _statesEmitPair    :: ! (Unboxed (Z:.PInt () StateIndex:.Letter RNA:.Letter RNA) Bitscore)
+  -- ^ Scores for the emission of a pair
+  , _statesEmitSingle  :: ! (Unboxed (Z:.PInt () StateIndex:.Letter RNA) Bitscore)
+  -- ^ Scores for the emission of a single nucleotide
+  }
+  deriving (Eq,Show,Read,Generic)
+
+makeLenses ''States
+makePrisms ''States
+
+instance Default States where
+  def = States
+    { _statesType        = fromAssocs 0            0            (StateType $ -1)  []
+    , _statesParents     = fromAssocs 0            0            VG.empty          []
+    , _statesTransitions = fromAssocs 0            0            VG.empty          []
+    , _statesQDB         = fromAssocs 0            0            def               []
+    , _statesEmitPair    = fromAssocs (Z:.0:.A:.A) (Z:.0:.A:.A) 0                 []
+    , _statesEmitSingle  = fromAssocs (Z:.0:.A)    (Z:.0:.A)    0                 []
+    }
+
+instance Binary    States
+instance Serialize States
+instance FromJSON  States
+instance ToJSON    States
+instance NFData    States
+
+-- | A pure getter to retrieve the last state
+
+sLastState :: Getter States (PInt () StateIndex)
+sLastState = statesType . to bounds . to snd
+{-# Inline sLastState #-}
+
+
+
+-- * Nodes for dynamically changeable models.
+
+-- | @Node@s are a high-level structure in covariance models, with each
+-- node having one or more states as children. In addition, nodes carry
+-- alignment-column based information.
+--
+-- TODO @_nColL@ and @nColR@ should become @Index 1@ types. We'll do that
+-- once we re-activate Stockholm file parsing.
+
+data Node = Node
+  { _nodeType   :: ! NodeType
+  -- ^ Type of the node
+  , _nodeStates :: ! (V.Vector (PInt () StateIndex))
+  -- ^ States associated with this node
+  , _nodeColL   :: ! Int
+  -- ^ Column index in the corresponding Stockholm file
+  , _nodeColR   :: ! Int
+  -- ^ Column index in the corresponding Stockholm file
+  , _nodeConL   :: ! Char
+  -- ^ TODO
+  , _nodeConR   :: ! Char
+  -- ^ TODO
+  , _nodeRefL   :: ! Char
+  -- ^ TODO
+  , _nodeRefR   :: ! Char
+  -- ^ TODO
+  }
+  deriving (Eq,Ord,Show,Read,Generic)
+
+makeLenses ''Node
+makePrisms ''Node
+
+instance Binary    Node
+instance Serialize Node
+instance FromJSON  Node
+instance ToJSON    Node
+instance NFData    Node
+
+
+
+-- * High-performance structure for @Node@s.
+
+data Nodes = Nodes
+  { _nodesType   :: ! (Unboxed (PInt () NodeIndex) NodeType)
+  , _nodesStates :: ! (Boxed   (PInt () NodeIndex) (V.Vector (PInt () StateIndex)))
+  , _nodesColL   :: ! (Unboxed (PInt () NodeIndex) Int)
+  , _nodesColR   :: ! (Unboxed (PInt () NodeIndex) Int)
+  , _nodesConL   :: ! (Unboxed (PInt () NodeIndex) Char)
+  , _nodesConR   :: ! (Unboxed (PInt () NodeIndex) Char)
+  , _nodesRefL   :: ! (Unboxed (PInt () NodeIndex) Char)
+  , _nodesRefR   :: ! (Unboxed (PInt () NodeIndex) Char)
+  }
+  deriving (Eq,Show,Read,Generic)
+
+makeLenses ''Nodes
+makePrisms ''Nodes
+
+instance Default Nodes where
+  def = Nodes
+    { _nodesType   = fromAssocs 0 0 (NodeType $ -1) []
+    , _nodesStates = fromAssocs 0 0 VG.empty        []
+    , _nodesColL   = fromAssocs 0 0 (-1)            []
+    , _nodesColR   = fromAssocs 0 0 (-1)            []
+    , _nodesConL   = fromAssocs 0 0 '-'             []
+    , _nodesConR   = fromAssocs 0 0 '-'             []
+    , _nodesRefL   = fromAssocs 0 0 '-'             []
+    , _nodesRefR   = fromAssocs 0 0 '-'             []
+    }
+
+instance Binary    Nodes
+instance Serialize Nodes
+instance FromJSON  Nodes
+instance ToJSON    Nodes
+instance NFData    Nodes
+
+
+
+
+data StaticModel = StaticModel
+  { _smStates :: ! States
+  , _smNodes  :: ! Nodes
+  }
+
+-- | Model structure that is somewhat easy to modify. Before turning this
+-- into a @StaticModel@, the model itself needs to be valid.
+
+data FlexibleModel = FlexibleModel
+  { _fmStates :: ! (Map (PInt () StateIndex) State)
+  , _fmNodes  :: ! (Map (PInt () NodeIndex ) Node )
+  }
+
+isValidModel :: FlexibleModel -> Bool
+isValidModel = error "isvalidModel: write me!"
+
+-- | Make a flexible model static.
+--
+-- TODO should *really* do some basic tests
+--
+-- TODO this would be easier if we introduced hybrid arrays, not just
+-- @Unboxed@ and @Boxed@. ... or if everything were unboxed.
+--
+-- TODO needs to generalize over the actual model we are dealing with. This
+-- includes how many characters to emit in pair and single. And the
+-- underlying alphabet.
+--
+-- TODO use @isValidModel@ for tests.
+
+flexibleToStatic :: FlexibleModel -> StaticModel
+flexibleToStatic (FlexibleModel s n)
+  | True = StaticModel s' n'
+  where s' = States
+          { _statesType        = fromAssocs 0 mix (StateType $ -1) $ zip ix $ s ^.. traverse . stateType
+          , _statesParents     = fromAssocs 0 mix VG.empty         $ zip ix $ s ^.. traverse . stateParents
+          , _statesTransitions = fromAssocs 0 mix VG.empty         $ zip ix $ s ^.. traverse . stateTransitions
+          , _statesQDB         = fromAssocs 0 mix def              $ zip ix $ s ^.. traverse . stateQDB
+          --
+          , _statesEmitPair    = fromAssocs (Z:.0:.A:.A) (Z:.mix:.U:.U) def $
+                [ (Z:.k:.n1:.n2,e)
+                | (k,es) <- zip ix $ s ^.. traverse . stateEmissions
+                , VG.length es == 16
+                , ((n1,n2),e) <- zip ((,) <$> acgu <*> acgu) (VG.toList es)
+                ]
+          , _statesEmitSingle  = fromAssocs (Z:.0:.A) (Z:.mix:.U) def $
+                [ (Z:.k:.n1,e)
+                | (k,es) <- zip ix $ s ^.. traverse . stateEmissions
+                , VG.length es == 4
+                , ((n1),e) <- zip acgu (VG.toList es)
+                ]
+          } where ix = M.keys s ; mix = maximum ix
+        n' = Nodes
+          { _nodesType   = fromAssocs 0 mix (NodeType $ -1) $ zip ix $ n ^.. traverse . nodeType
+          , _nodesStates = fromAssocs 0 mix VG.empty        $ zip ix $ n ^.. traverse . nodeStates
+          , _nodesColL   = fromAssocs 0 mix (-1)            $ zip ix $ n ^.. traverse . nodeColL
+          , _nodesColR   = fromAssocs 0 mix (-1)            $ zip ix $ n ^.. traverse . nodeColR
+          , _nodesConL   = fromAssocs 0 mix '-'             $ zip ix $ n ^.. traverse . nodeConL
+          , _nodesConR   = fromAssocs 0 mix '-'             $ zip ix $ n ^.. traverse . nodeConR
+          , _nodesRefL   = fromAssocs 0 mix '-'             $ zip ix $ n ^.. traverse . nodeRefL
+          , _nodesRefR   = fromAssocs 0 mix '-'             $ zip ix $ n ^.. traverse . nodeRefR
+          } where ix = M.keys n ; mix = maximum ix
+
+{-
 
 -- *
 
@@ -386,4 +475,5 @@ instance Serialize ModelStruct
 instance FromJSON  ModelStruct
 instance ToJSON    ModelStruct
 instance NFData    ModelStruct
+-}
 
